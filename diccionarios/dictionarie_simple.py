1a92be6ba5810537d4babfebe51945c1158a64a1
Sebastian Medina <JSebastian19952011@gmail.com>
2025-12-03 00:03:28 -0500

    Practicando diccionarios

---
+++ diccionarios/dictionarie_simple.py
@@ -0,0 +1,19 @@
+
+# Básicamente Clave - Valor
+dictionary_example = {
+    "3bbb2acf-d593-4647-bdd0-a50c75bc087c": "3126721109",
+    "0ea3b7e8-8dcd-4475-862e-054675158032": "3127849302",
+    "edda724f-d6ec-43ef-a777-eddaa720fa0b": "3128739994",
+    "c6a1f701-371c-433e-b7ae-eba63acad234": "3118292093",
+    "ebee4590-ee95-4794-a092-96d032b73071": "3148920923"
+}
+
+usuario = {
+    "id": 1,
+    "name": "Sebas",
+    "email": "sebas@example.com",
+    "role": "admin"
+}
+
+# Accedo a la llave requerida
+print(usuario["name"])
