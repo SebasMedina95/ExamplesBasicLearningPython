2649f9baea6ec75ae80386b86ffe842a57d9b3c3
Sebastian Medina <JSebastian19952011@gmail.com>
2025-12-05 22:48:38 -0500

    Practicando sets

---
+++ tuples_sets/sets.py
@@ -0,0 +1,9 @@
+
+# Colección desordenada
+# No tiene indices
+# No permite duplicados
+# Optico operaciones matematicas
+emails = ["a@mail.com", "b@mail.com", "a@mail.com", "c@mail.com"]
+
+unic_emails = list(set(emails))
+print(unic_emails)
